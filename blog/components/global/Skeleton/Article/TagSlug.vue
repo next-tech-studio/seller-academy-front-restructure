c99b21ce3bf6434eb1dc14b8b64db953883135eb
niloofar <niloofartahmaseb.diginext@gmail.com>
2024-02-10 11:20:33 +0330

    add: skeleton to blog modules

---
+++ blog/components/global/Skeleton/Article/TagSlug.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-skeleton-loader
+          width="20%"
+          type="subtitle, heading"
+        ></v-skeleton-loader>
+      </v-col>
+      <v-col cols="12" md="4" v-for="n in 9" :key="n" class="mb-9">
+        <v-skeleton-loader
+          dir="ltr"
+          type="card-avatar, article"
+        ></v-skeleton-loader>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
