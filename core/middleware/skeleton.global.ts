4681b0e41f2959ff035df28d5649be7d5fdb81ff
niloofar <niloofartahmaseb.diginext@gmail.com>
2024-02-10 11:21:06 +0330

    fix: skeleton middleware

--- core/middleware/skeleton.global.ts
+++ core/middleware/skeleton.global.ts
@@ -4,11 +4,11 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const route = useRoute()
   
   let skeleton = "";
-  if (to.name == "blog") skeleton = "skeleton-blog";
+  if (route.name == "blog") skeleton = "skeleton-blog";
   else if (route.name == "article-archive") skeleton = "skeleton-article-archive";
   else if (route.name == "article-category-slug") skeleton = "skeleton-article-category-slug";
   else if (route.name == "article-tag-slug") skeleton = "skeleton-article-tag-slug";
-  else skeleton = ''
+  else if (route.name == "podcast-archive") skeleton = "skeleton-podcast-archive";
   if (skeleton && !process.server) {
     store.skeleton = skeleton;
     store.skeletonLoading = true && skeleton;
