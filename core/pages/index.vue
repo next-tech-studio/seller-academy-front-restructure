f888c3fcd1b0b7955bbdb6910366a36dc716a4a1
niloofar <niloofartahmaseb.diginext@gmail.com>
2024-02-19 11:01:51 +0330

    fix: homepage initial loading

--- core/pages/index.vue
+++ core/pages/index.vue
@@ -1,83 +1,90 @@
 <template>
-  <div v-if="!auth?.user?.loggedIn">
-    <landing-header />
-    <landing-calculator :items="incomes" />
-    <landing-quote class="my-10 py-lg-10 py-0" />
-    <v-container v-if="articles.length">
-      <app-content-card-listing
-        :content="articles.slice(0, 4)"
-        title="relevant_content"
-        :responsive-horizontal="true"
-        @to:item="toItem($event)"
-        :show-see-more="false"
-        :show-filter="false"
-        grid="3"
-      ></app-content-card-listing>
-    </v-container>
-    <landing-roadmap />
-    <landing-stories />
-  </div>
-  <div v-else>
-    <v-container>
-      <v-row>
-        <v-col>
-          <v-card flat :height="lgAndUp ? 380 : 205" width="100%" class="mb-8 mt-lg-8 mt-4">
-          <v-img
-            height="100%"
-            width="100%"
-            cover
-            src="/images/loggedin-landing-header.png"
-          ></v-img>
-        </v-card>
-        </v-col>
-      </v-row>
-      <app-content-card-listing
-        :class="isClient ? 'd-flex' : 'd-none'"
-        title="latest_articles_and_news"
-        :content="articles?.slice(0, 3)"
-        :responsive-horizontal="true"
-        @to:item="toItem($event)"
-        @load:more="navigateTo(localePath({ name: 'article-archive' }))"
-        see-more-title="see_more"
-        :showFilter="false"
-      />
-      <course-landing-our-courses
-        class="my-10"
-        :categories="categories"
-      ></course-landing-our-courses>
-      <client-only>
-        <h2 class="text-h2 text-text-high-emphasis mb-6">
-          {{ $t("most_popular_rooms") }}
-        </h2>
-        <app-switch-wrapper class="pa-0 me-0" one-side-margin>
-          <v-slide-group>
-            <v-slide-group-item
-              v-for="(item, index) in rooms"
-              :key="index"
-              :value="item.slug"
+  <div>
+    <template v-if="!auth?.user?.loggedIn">
+      <landing-header />
+      <landing-calculator :items="incomes" />
+      <landing-quote class="my-10 py-lg-10 py-0" />
+      <v-container v-if="articles.length">
+        <app-content-card-listing
+          :content="articles.slice(0, 4)"
+          title="relevant_content"
+          :responsive-horizontal="true"
+          @to:item="toItem($event)"
+          :show-see-more="false"
+          :show-filter="false"
+          grid="3"
+        ></app-content-card-listing>
+      </v-container>
+      <landing-roadmap />
+      <landing-stories />
+    </template>
+    <template v-else>
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-card
+              flat
+              :height="lgAndUp ? 380 : 205"
+              width="100%"
+              class="mb-8 mt-lg-8 mt-4"
             >
-              <div class="me-2">
-                <room-card
-                  :item="item"
-                  @to:item="toRoom"
-                  :max-width="296"
-                ></room-card>
-              </div>
-            </v-slide-group-item>
-          </v-slide-group>
-        </app-switch-wrapper>
-      </client-only>
-      <v-row class="mb-10">
-        <v-btn
-          flat
-          @click="navigateTo(localePath({ name: 'forum-room-list' }))"
-          class="align-self-center text-button mt-6 mx-auto"
-          height="48px"
-          color="primary-base"
-          >{{ $t("see_more") }}
-        </v-btn>
-      </v-row>
-    </v-container>
+              <v-img
+                height="100%"
+                width="100%"
+                cover
+                src="/images/loggedin-landing-header.png"
+              ></v-img>
+            </v-card>
+          </v-col>
+        </v-row>
+        <app-content-card-listing
+          :class="isClient ? 'd-flex' : 'd-none'"
+          title="latest_articles_and_news"
+          :content="articles?.slice(0, 3)"
+          :responsive-horizontal="true"
+          @to:item="toItem($event)"
+          @load:more="navigateTo(localePath({ name: 'article-archive' }))"
+          see-more-title="see_more"
+          :showFilter="false"
+        />
+        <course-landing-our-courses
+          class="my-10"
+          :categories="categories"
+        ></course-landing-our-courses>
+        <client-only>
+          <h2 class="text-h2 text-text-high-emphasis mb-6">
+            {{ $t("most_popular_rooms") }}
+          </h2>
+          <app-switch-wrapper class="pa-0 me-0" one-side-margin>
+            <v-slide-group>
+              <v-slide-group-item
+                v-for="(item, index) in rooms"
+                :key="index"
+                :value="item.slug"
+              >
+                <div class="me-2">
+                  <room-card
+                    :item="item"
+                    @to:item="toRoom"
+                    :max-width="296"
+                  ></room-card>
+                </div>
+              </v-slide-group-item>
+            </v-slide-group>
+          </app-switch-wrapper>
+        </client-only>
+        <v-row class="mb-10">
+          <v-btn
+            flat
+            @click="navigateTo(localePath({ name: 'forum-room-list' }))"
+            class="align-self-center text-button mt-6 mx-auto"
+            height="48px"
+            color="primary-base"
+            >{{ $t("see_more") }}
+          </v-btn>
+        </v-row>
+      </v-container>
+    </template>
   </div>
 </template>
 
