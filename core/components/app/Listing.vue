7b1ca33505fd49c1d708fea5a1f0529b0dc8d5d0
niloofar <niloofartahmaseb.diginext@gmail.com>
2024-02-20 18:02:56 +0330

    add: listing btn bug and redirect

--- core/components/app/Listing.vue
+++ core/components/app/Listing.vue
@@ -125,15 +125,18 @@
               <span v-if="!column.sortable && !column.selectAll">{{
                 column?.title
               }}</span>
-              <v-checkbox-btn
+              <v-checkbox
                 v-if="column.selectAll && selectable"
                 true-icon="custom:squareCheck"
                 false-icon="custom:square"
                 :model-value="allSelected"
-                :label="column?.title"
                 @update:model-value="selectAll"
+                hide-details
               >
-              </v-checkbox-btn>
+              <template #label>
+                <span variant="text" class="ms-4">{{ column?.title }}</span>
+              </template>
+              </v-checkbox>
               <div class="d-flex">
                 <span
                   v-if="column.sortable"
@@ -162,28 +165,33 @@
           :header="{ header }"
           class="pe-10"
         >
-          <div class="d-flex cursor-pointer" v-if="header.key == 'title'">
-            <div
+          <!-- <div class="cursor-pointer" v-if="header.key == 'title'"> -->
+            <!-- <div
               class="d-flex align-center justify-start"
               :style="`width: ${header.size} !important; flex: 0 1 0%`"
+            > -->
+            <v-checkbox
+              v-if="selectable && header.key == 'title'"
+              true-icon="custom:squareCheck"
+              false-icon="custom:square"
+              :value="item"
+              multiple
+              v-model="store.selectedTableItems"
+              hide-details
             >
-              <v-checkbox
-                v-if="selectable"
-                true-icon="custom:squareCheck"
-                false-icon="custom:square"
-                :value="item"
-                multiple
-                v-model="store.selectedTableItems"
-                hide-details
-              >
               <template #label>
-                <span class="text-truncate text-body-1" @click="goToItem(item)">{{
-                item[header.key]
-              }}</span>
+                <v-btn
+                  @click="goToItem(item)"
+                  variant="text"
+                  :ripple="false"
+                  class="text-truncate text-body-1"
+                >
+                  {{ item[header.key] }}
+                </v-btn>
               </template>
-              </v-checkbox>
-            </div>
-          </div>
+            </v-checkbox>
+            <!-- </div> -->
+          <!-- </div> -->
           <div v-if="header.key == 'author'" style="width: fit-content">
             <span class="text-body-1">{{ item.author.displayName }}</span>
           </div>
@@ -387,7 +395,7 @@ const emit = defineEmits([
   "show:dialog",
   "navigate:toItem",
   "update:hardDelete",
-  "update:mostPopular"
+  "update:mostPopular",
 ]);
 let pageNumber = computed({
   get() {
@@ -438,7 +446,7 @@ const edit = (item, action) => {
 };
 defineExpose({ edit, changeItemStatus });
 const goToItem = (item) => {
-  props.store.currentItem = item.value;
+  props.store.currentItem = item;
   emit("navigate:toItem");
 };
 const operateGroupAction = (action) => {
