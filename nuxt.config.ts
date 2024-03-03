9125ff0d51a838e261a00f34fb132dc820e219a2
niloofar <niloofartahmaseb.diginext@gmail.com>
2024-03-03 12:29:09 +0330

    fix: Text compression lighthouse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: [process.env.CORE.substring(1), "./forum", "./blog", "./academy"],
+  nitro: {
+    compressPublicAssets: true,
+  }
 });
