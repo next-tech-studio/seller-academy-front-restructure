4dcd21ce469584b78e3d0678f3fe519d495cab0e
HaniehMa <hanieh.arzanagh@gmail.com>
2024-03-04 18:31:26 +0330

    add: client join request

--- core/middleware/roles.ts
+++ core/middleware/roles.ts
@@ -2,6 +2,7 @@ import { useAuthStore } from "@core/stores/auth";
 export default defineNuxtRouteMiddleware(
   (to, from) => {
     const auth = useAuthStore();
+    console.log('oeoeooe',from)
     const requiredPermissions = to.meta.permissions || [];
     if (!auth.hasPermission(requiredPermissions)) {
       return navigateTo('/login');
